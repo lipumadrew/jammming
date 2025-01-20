5a0cfd7c58cb15655e7c1eb7d5e0f53f836140ba
Andrew Lipuma <andrewlipuma@gmail.com>
2025-01-19 22:17:31 -0800

    Add CSS modules for PlaylistEditor, SearchBar, and SearchResults components

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import SearchBar from "./components/SearchBar.jsx";
+import SearchBar from "./components/SearchResults.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
