25af364bf8b144ee8774485be747d5af026b231b
Andrew Lipuma <andrewlipuma@gmail.com>
2025-01-21 22:03:53 -0800

    Add some styling to the artist cards similar to the track cards

--- src/components/Artist.jsx
+++ src/components/Artist.jsx
@@ -1,6 +1,27 @@
 import styles from "../css/Artist.module.css";
+import { useState } from "react";
 
 const Artist = (props) => {
+    const [name, setName] = useState(props.artistName);
+    const [genres, setGenres] = useState(props.artistGenres);
+
+    let genreString = "";
+    genres.map((genre, idx) => {
+        genreString += genre
+        if (idx < genres.length - 1) {
+            genreString += ","
+        }
+    })
+
+    let nameString = name;
+    if (nameString.length > 14) {
+        nameString = nameString.slice(0,14) + "...";
+    }
+
+    if (genreString.length > 15) {
+        genreString = genreString.slice(0,15) + "...";
+    }
+
   return (
     <div id="artist-container" className={styles.artistContainer}>
       <div id="artist-img-container" className={styles.artistImgContainer}>
@@ -10,8 +31,8 @@ const Artist = (props) => {
         id="artist-details-container"
         className={styles.artistDetailsContainer}
       >
-        <h2 className={styles.artistTitle}>{props.artistName}</h2>
-        <h3 className={styles.artistFollowers}>{props.artistFollowers} Followers</h3>
+        <h2 className={styles.artistTitle}>{nameString}</h2>
+        <h3 className={styles.artistFollowers}>{genreString}</h3>
       </div>
     </div>
   );
